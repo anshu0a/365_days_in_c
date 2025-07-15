b145bd0b73c3372f65a5d47001421996e5c82968
anshu <147285931+anshu0a@users.noreply.github.com>
2025-07-15 19:35:07 +0530

    Update day4_of_365.c
    2nd question added

--- day4_of_365.c
+++ day4_of_365.c
@@ -26,3 +26,41 @@ end:
 printf("%s",ch);
     return 0;
 }
+
+
+
+// [Title: Electricity Bill Calculator -Using else-if ladder]
+// ----------------------------------------------------------
+// Write a C program to input electricity unit charges and calculate the total electricity bill according to the following conditions:[using ternary-operator]
+
+// -> For the first 50 units: Rs. 0.50 per unit
+// -> For the next 100 units: Rs. 0.75 per unit
+// -> For the next 100 units: Rs. 1.20 per unit
+// -> For units above 250: Rs. 1.50 per unit
+
+// Additionally, a surcharge of 20% is added to the bill.
+
+// Sample Input :-
+// ---------------
+// Enter the number of units: 280
+
+// Sample Output :-
+// -----------------
+// Total electricity bill: Rs. 318.00
+
+
+#include<stdio.h>
+int main(){
+    int a;
+    float r;
+    scanf("%d",&a);
+    
+    if( a <= 50) r = a*(0.50f);
+    else if( a <= 150) r = ( 50*0.50 +(a-50)*(0.75f));
+    else if( a <= 250)  r =( 50*0.50 + (100)*(0.75f) +(a-150)*(1.20f));
+    else r =  50*0.50 + (100)*(0.75f) +(100)*(1.20f) + (a - 250)*(1.50f);
+
+    printf("Total electricity bill: Rs. %.2f", (r + (r *.2)));
+
+    return 0;
+}
